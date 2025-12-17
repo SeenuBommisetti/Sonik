de04f77308257c0e97e8164c606975eb36ec68ac
Seenu-Bommisetti <bommisettiseenu@gmail.com>
2025-12-17 20:48:10 +0530

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/seenubommisetti/sonik/Greeting.kt
@@ -0,0 +1,9 @@
+package com.seenubommisetti.sonik
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/seenubommisetti/sonik/Platform.kt
@@ -0,0 +1,7 @@
+package com.seenubommisetti.sonik
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
