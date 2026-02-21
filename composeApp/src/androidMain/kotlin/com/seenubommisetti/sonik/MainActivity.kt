5208ec37caa74fe1fcf74f660fbaae62617d3234
Seenu-Bommisetti <bommisettiseenu@gmail.com>
2026-02-21 19:47:58 +0530

    Migrated Compose UI to Compose Multiplatform

--- composeApp/src/androidMain/kotlin/com/seenubommisetti/sonik/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/seenubommisetti/sonik/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
-import com.seenubommisetti.sonik.ui.TrackListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
